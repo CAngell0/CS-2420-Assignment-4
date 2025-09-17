61296b8419564e91330cc733e5eea3f0df847fed
Carson Angell <carsonjesseangell@gmail.com>
2025-09-16 23:05:32 -0600

    Finished the getSimilarityGroups method and changed the StringSimilarityGroupComparator to use the StringNumericalValueComparator with the findMax method (per assignment requirements)

--- app/src/main/java/assign04/IntegerStringUtility.java
+++ app/src/main/java/assign04/IntegerStringUtility.java
@@ -7,7 +7,10 @@ import java.util.NoSuchElementException;
 
 public class IntegerStringUtility {
     public static void main(String[] args) {
-        getSimilarityGroups(new String[]{"2341", "123", "2134", "2431", "312", "2143"});
+        String[][] arr = getSimilarityGroups(new String[]{"2341", "123", "2134", "2431", "312", "2143"});
+        for (String[] row : arr){
+            System.out.println(Arrays.toString(row));
+        }
     }
 
     public static <E> void insertionSort(E[] arr, Comparator<? super E> cmp){
@@ -42,18 +45,7 @@ public class IntegerStringUtility {
     public static class StringNumericalValueComparator implements Comparator<String> {
         @Override
         public int compare(String o1, String o2) {
-            //TODO : test edge cases carefully
-            //TODO : see if we actually need to implement these testings
-            // try {
-            //     Integer.valueOf(o1); Integer.valueOf(o2);
-            // }
-            // catch (NumberFormatException error){
-            //     return null;
-            // }
-            
-            // if(o1.contains('-') == '-' || o2.contains('-')){
-            //     return null; 
-            // }
+            //TODO : test
             String o1Stripped = stripLeadingZeros(o1);
             String o2Stripped = stripLeadingZeros(o2);
             if (o1Stripped.length() != o2Stripped.length()) return Integer.compare(o1Stripped.length(), o2Stripped.length());
@@ -79,7 +71,12 @@ public class IntegerStringUtility {
             insertionSort(charArray1, (char1, char2) -> char1.compareTo(char2));
             insertionSort(charArray2, (char1, char2) -> char1.compareTo(char2));
 
-            return charArray1.toString().compareTo(charArray2.toString());
+            StringBuilder o1StrBuilder = new StringBuilder();
+            for (Character charEl : charArray1) o1StrBuilder.append(charEl.charValue());
+            StringBuilder o2StrBuilder = new StringBuilder();
+            for (Character charEl : charArray2) o2StrBuilder.append(charEl.charValue());
+
+            return o1StrBuilder.toString().compareTo(o2StrBuilder.toString());
         }
     }
 
@@ -88,17 +85,18 @@ public class IntegerStringUtility {
         public int compare(String[] o1, String[] o2) {
             //TODO - Test that this method works.
             int lengthComparison = Integer.compare(o1.length, o2.length);
-
             if (lengthComparison != 0) return lengthComparison;
             else {
-                String o1Max = findMax(o1, (s1, s2) -> s1.compareTo(s2));
-                String o2Max = findMax(o2, (s1, s2) -> s1.compareTo(s2));
+                StringNumericalValueComparator cmp = new StringNumericalValueComparator();
+                String o1Max = findMax(o1, cmp);
+                String o2Max = findMax(o2, cmp);
                 return o1Max.compareTo(o2Max);
             }
         }
     }
 
     public static String[][] getSimilarityGroups(String[] arr){
+        //TODO - Test
         String[] arrCopy = Arrays.copyOf(arr, arr.length);
 
         StringSimilarityComparator cmp = new StringSimilarityComparator();
@@ -106,56 +104,19 @@ public class IntegerStringUtility {
 
         ArrayList<String[]> similarityGroups = new ArrayList<>();
         int groupStartIndex = 0;
-        int groupEndIndex = 1;
-        for (int i = 0; i < arrCopy.length - 1; i++){
-            if (cmp.compare(arrCopy[i], arrCopy[i + 1]) != 0){
-                String[] group = new String[groupEndIndex - groupStartIndex];
+        for (int i = 0; i <= arrCopy.length; i++){
+            if (i != arrCopy.length && cmp.compare(arrCopy[groupStartIndex], arrCopy[i]) == 0) continue;
 
-                for (int j = groupStartIndex; j < groupEndIndex; j++){
-                    group[j - groupStartIndex] = arrCopy[groupStartIndex];
-                }
-
-                groupStartIndex = groupEndIndex;
-                similarityGroups.add(group);
+            String[] group = new String[i - groupStartIndex];
+            for (int j = groupStartIndex; j < i; j++){
+                group[j - groupStartIndex] = arrCopy[j];
             }
-            groupEndIndex++;
+
+            groupStartIndex = i;
+            similarityGroups.add(group);
         }
 
-        similarityGroups.forEach((strArr) -> {
-            System.out.println(Arrays.toString(strArr));
-        });
-        // return null;
-        /*
-         * Keep a count of how many times a element repeats and keep a count of how many elements there are
-         * How many times max element apears is [][here]
-         * How many elements we have is [here][]
-         * loop through the array and when i = #times the first element repeats move into the next row [][+1]
-         * need to figure out how to store how many copies of each element
-         */
-        
-         //[element #][count]
-        // int counter[] = new int[arr.length];
-        // int size = 0;
-        // int count = 1;
-
-        // for(int i = 1; i < arr.length; i++){
-        //     if(arrCopy[i] == arrCopy[i-1]){
-        //         count++;
-        //     }
-        //     counter[size] = count;
-        //     size++;
-        //     count = 0;
-        // }
-        // String[][] sortedStrings = new String[1][counter.length];
-        
-        // for(int i = 0; i < arr.length; i++){
-        //     /**
-        //      * Keep putting the elements in the same row as long as counter[?] != 0
-        //      */
-        // }
-
-        return null;
-
+        return similarityGroups.toArray(new String[0][0]);
     }
 
     public static String[] findMaximumSimilarityGroup(int[] arr) throws NoSuchElementException {
