f9926c38ef5fbb0449b65e3b101191888865c8ea
Carson Angell <carsonjesseangell@gmail.com>
2025-09-16 20:24:39 -0600

    Completed similarity group comparator and started the getSimilarityGroups method

--- app/src/main/java/assign04/IntegerStringUtility.java
+++ app/src/main/java/assign04/IntegerStringUtility.java
@@ -1,27 +1,38 @@
 package assign04;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
 public class IntegerStringUtility {
-    public static <E> void insert(E[] arr, E element,  Comparator<? super E> cmp, int upperIndex){
-        //TODO - Check for invalid upperIndex values
-        for (int i = upperIndex - 1; i >= 1; i--){
-            if (cmp.compare(arr[i - 1], element) > 0){
-                E temp = arr[i - 1];
-                arr[i - 1] = element;
-                arr[i] = temp;
-            }
-        }
-    }
+    // public static <E> void insert(E[] arr, E element,  Comparator<? super E> cmp, int upperIndex){
+    //     //TODO - Check for invalid upperIndex values
+    //     for (int i = upperIndex - 1; i >= 1; i--){
+    //         if (cmp.compare(arr[i - 1], element) > 0){
+    //             E temp = arr[i - 1];
+    //             arr[i - 1] = element;
+    //             arr[i] = temp;
+    //         }
+    //     }
+    // }
 
     public static <E> void insertionSort(E[] arr, Comparator<? super E> cmp){
         //TODO - Test this method
-        for (int i = 0; i < arr.length; i++){
-            for (int j = i; j < arr.length; j++){
-                insert(arr, arr[j], cmp, i);
+        for (int i = 1; i < arr.length; i++){
+            E key = arr[i];
+            int j = i - 1;
+            while (j <= 0 && cmp.compare(arr[j], key) > 0){
+                arr[j + 1] = arr[j];
+                j--;
             }
+
+            arr[j + 1] = key;
         }
+        // for (int i = 0; i < arr.length; i++){
+        //     for (int j = i; j < arr.length; j++){
+        //         insert(arr, arr[j], cmp, i);
+        //     }
+        // }
     }
 
     public static <E> E findMax(E[] arr, Comparator<? super E> cmp) throws NoSuchElementException {
@@ -65,7 +76,7 @@ public class IntegerStringUtility {
         @Override
         public int compare(String o1, String o2) {
             //TODO: test to make sure this all works
-            if(o1.length() != o2.length()) return Integer.valueOf(o1.length()).compareTo(Integer.valueOf(o2.length()));
+            if(o1.length() != o2.length()) return Integer.compare(o1.length(), o2.length());
             Character[] charArray1 = splitStringToCharacterArray(o1);
             Character[] charArray2 = splitStringToCharacterArray(o2);
             insertionSort(charArray1, (char1, char2) -> char1.compareTo(char2));
@@ -78,11 +89,24 @@ public class IntegerStringUtility {
     public static class StringSimilarityGroupComparator implements Comparator<String[]> {
         @Override
         public int compare(String[] o1, String[] o2) {
-            return 0;
+            //TODO - Test that this method works.
+            int lengthComparison = Integer.compare(o1.length, o2.length);
+
+            if (lengthComparison != 0) return lengthComparison;
+            else {
+                String o1Max = findMax(o1, (s1, s2) -> s1.compareTo(s2));
+                String o2Max = findMax(o2, (s1, s2) -> s1.compareTo(s2));
+                return o1Max.compareTo(o2Max);
+            }
         }
     }
 
     public static String[][] getSimilarityGroups(String[] arr){
+        String[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Comparator<String> similarityComparator = new StringSimilarityComparator();
+        insertionSort(arrCopy, similarityComparator);
+        
         return null;
     }
 
