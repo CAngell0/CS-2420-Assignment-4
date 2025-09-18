b45ad3e5397ddd0d62d116e3feef5718eb13cbb9
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-09-18 13:39:14 -0600

    Added timing expirement for the original findMax method

---
+++ app/src/main/java/timing/OriginalFindMaxExpirement.java
@@ -0,0 +1,40 @@
+package timing;
+
+import java.util.List;
+
+import assign04.IntegerStringUtility;
+
+public class OriginalFindMaxExpirement extends TimingExperiment {
+    private Integer[] numbers;
+
+    public static void main(String[] args) {
+        int iterationCount = 50;
+        String problemSizeName = "Original Find Max Method";
+        List<Integer> problemSizes = buildProblemSizes(1000, 1000, 20);
+
+        TimingExperiment experiment = new OriginalFindMaxExpirement(problemSizeName, problemSizes, iterationCount);
+
+        experiment.warmup(50);
+        experiment.run();
+        experiment.print();
+        experiment.writeToCSV("timing.csv");
+    }
+
+    public OriginalFindMaxExpirement(String problemSizeName, List<Integer> problemSizes, int iterationCount){
+        super(problemSizeName, problemSizes, iterationCount);
+        
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        numbers = new Integer[problemSize];
+        for (int index = 0; index < problemSize; index++){
+            numbers[index] = Integer.valueOf((int) Math.random() * 1000);
+        }
+    }
+
+    @Override
+    protected void runComputation() {
+        IntegerStringUtility.findMax(numbers, (n1, n2) -> Integer.compare(n1, n2));
+    }
+}
