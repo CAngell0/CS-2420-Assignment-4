cf09df36d710abbb54de6c6f22569d9dff4ee4b5
shuazs <varughesejoshua50@gmail.com>
2025-09-18 17:50:38 -0600

    Made all the classes

---
+++ app/src/main/java/assign04/InsertionSortAverageCaseTimingExperiment.java
@@ -0,0 +1,24 @@
+package assign04;
+
+import java.util.List;
+
+public class InsertionSortAverageCaseTimingExperiment extends ArraySortTimingExperiment{
+
+    public InsertionSortAverageCaseTimingExperiment(List<Integer> problemSizes, int iterationCount) {
+        super(problemSizes, iterationCount);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+    }
+
+    @Override
+    protected void runComputation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+    }
+
+}
---
+++ app/src/main/java/assign04/InsertionSortBestCaseTimingExperiment.java
@@ -0,0 +1,24 @@
+package assign04;
+
+import java.util.List;
+
+public class InsertionSortBestCaseTimingExperiment extends ArraySortTimingExperiment{
+
+    public InsertionSortBestCaseTimingExperiment(List<Integer> problemSizes, int iterationCount) {
+        super(problemSizes, iterationCount);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+    }
+
+    @Override
+    protected void runComputation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+    }
+
+}
