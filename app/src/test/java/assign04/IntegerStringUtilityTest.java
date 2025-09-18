3eccd83ef48de8c20b532e940a8acfbec15ede6d
shuazs <varughesejoshua50@gmail.com>
2025-09-17 21:09:12 -0600

    test

--- app/src/test/java/assign04/IntegerStringUtilityTest.java
+++ app/src/test/java/assign04/IntegerStringUtilityTest.java
@@ -1,5 +1,6 @@
 package assign04;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -149,4 +150,14 @@ public class IntegerStringUtilityTest {
             IntegerStringUtility.findMaximumSimilarityGroup(emptyArr);
         });
     }
+
+    @Test
+    void testInsertionSortEmptyArray() {
+        String[] emptyArr = {};
+        assertDoesNotThrow(() -> {
+            IntegerStringUtility.insertionSort(emptyArr, String::compareTo);
+        });
+    }
+    
+
 }
